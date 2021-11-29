255aa3c4e91f4a48ef3b048ffca92aaa9fa3c703
Christofer Cappeliez <c.cappeliez@orpea.net>
2021-11-29 16:24:01 +0100

    path

--- src/LdapOauthAuthServiceProvider.php
+++ src/LdapOauthAuthServiceProvider.php
@@ -25,10 +25,10 @@ class LdapOauthAuthServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/config/connect_settings.php' => config_path('connect_settings.php'),
-            __DIR__.'/Controllers/Auth' => app_path('Http/Controllers/Auth/Authentication.php'),
-            __DIR__.'/Controllers/Auth' => app_path('Http/Controllers/Auth/Ldap.php'),
-            __DIR__.'/Controllers/Auth' => app_path('Http/Controllers/Auth/LoginController.php'),
-            __DIR__.'/Controllers/Auth' => app_path('Http/Controllers/Auth/Oauth.php'),
+            __DIR__.'/Controllers/Auth/Authentication.php' => app_path('Http/Controllers/Auth/Authentication.php'),
+            __DIR__.'/Controllers/Auth/Ldap.php' => app_path('Http/Controllers/Auth/Ldap.php'),
+            __DIR__.'/Controllers/Auth/LoginController.php' => app_path('Http/Controllers/Auth/LoginController.php'),
+            __DIR__.'/Controllers/Auth/Oauth.php' => app_path('Http/Controllers/Auth/Oauth.php'),
         ], 'ldap-oauth');
     }
 }
